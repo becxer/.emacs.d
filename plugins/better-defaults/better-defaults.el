fbc8932ce19e231a5f21deeb29a1eb1689f5fd9f
becxer <becxer87@gmail.com>
2016-03-30 02:44:09 +0900

    add goto-line shortcut

--- plugins/better-defaults/better-defaults.el
+++ plugins/better-defaults/better-defaults.el
@@ -73,6 +73,7 @@
   (global-set-key (kbd "C-r") 'isearch-backward-regexp)
   (global-set-key (kbd "C-M-s") 'isearch-forward)
   (global-set-key (kbd "C-M-r") 'isearch-backward)
+  (global-set-key (kbd "C-l") 'goto-line)
 
   (show-paren-mode 1)
   (setq-default indent-tabs-mode nil)
